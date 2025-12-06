29927b58bd3698af24293c41ef758b622c051cc9
Minh Triet <phoenixmt1705@gmail.com>
2025-12-06 19:35:09 +0700

    chore(setup): init Spring Boot modular monolith structure

---
+++ src/test/java/com/clienthub/client_hub_backend/ClientHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clienthub.client_hub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
