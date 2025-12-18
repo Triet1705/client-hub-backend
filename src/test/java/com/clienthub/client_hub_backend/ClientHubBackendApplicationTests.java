ebe6f43d9ceddb60e6219ee93874239a4a0c0202
Minh Triet <phoenixmt1705@gmail.com>
2025-12-18 17:49:06 +0700

    Update test to exclude JPA auto-configuration and avoid database requirement

--- src/test/java/com/clienthub/client_hub_backend/ClientHubBackendApplicationTests.java
+++ src/test/java/com/clienthub/client_hub_backend/ClientHubBackendApplicationTests.java
@@ -1,19 +1,14 @@
 package com.clienthub.client_hub_backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
-@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 class ClientHubBackendApplicationTests {
-
-	@Container
-	@ServiceConnection
-	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
 
 	@Test
 	void contextLoads() {
